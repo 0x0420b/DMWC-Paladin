0bd09f8fb9c2a887671c53df8b173516d581ffc6
stefan <s.dapper2812@gmail.com>
2019-09-09 13:06:25 +0200

    MoreFunctionality (STAGE 1)

--- Rotation.lua
+++ Rotation.lua
@@ -43,33 +43,59 @@ local function Debugsettings()
 end
 
 local function regularCast(spell, Unit)
-	if Spell[spell]:Known() then
-		if Spell[spell]:Cast(Unit) then
-			return true
-		end
+	if Spell[spell]:Cast(Unit) then
+		return true
 	end
 end
 
-local function Buffing()
+local function Buffing()	
 	--Apply Aura
-	if Setting("Use Devotion Aura") and not Buff.DevotionAura:Exist() then
+	if Setting("Use Devotion Aura") and Spell.DevotionAura:Known() and not Buff.DevotionAura:Exist() then
 		regularCast("DevotionAura",Player)
 	end
-	if Setting("Use Retribution Aura") and not Buff.RetriAura:Exist() then
+	if Setting("Use Retribution Aura")and Spell.RetriAura:Known() and not Buff.RetriAura:Exist() then
 		regularCast("RetriAura",Player)
 	end
+	if Setting("Use Sanctity Aura")and Spell.SanctityAura:Known() and not Buff.SanctityAura:Exist() then
+		regularCast("SanctityAura",Player)
+	end
 	--Buff Self
-	if Setting("Use Blessing of Might") then
+	if Setting("Use Blessing of Might") and Spell.BlessingMight:Known() then
 		if not Buff.BlessingMight:Exist(Player) then
-			if regularCast("BlessingMight", Player) then
-				return true
+			if Spell.BlessingofMightG:Known() and not Buff.BlessingofMightG:Exist() then
+				if regularCast("BlessingofMightG", Player) then
+					return true
+				end
+			else 
+				if regularCast("BlessingMight", Player) then
+					return true
+				end
 			end
 		end
 	end
-	if Setting("Use Blessing of Wisdom") then
-		if not Buff.BlessingWissdom:Exist(Player) then
-			if regularCast("BlessingWissdom", Player) then
-				return true
+	if Setting("Use Blessing of Wisdom") and Spell.BlessingWisdom:Known() then
+		if not Buff.BlessingWisdom:Exist(Player) then
+			if Spell.BlessingofWisdomG:Known() and Buff.BlessingofWisdomG:Exist() then
+				if regularCast("BlessingofWisdomG", Player) then
+					return true
+				end
+			else
+				if regularCast("BlessingWisdom", Player) then
+					return true
+				end
+			end
+		end
+	end
+	if Setting("Use Blessing of Kings") then
+		if not Buff.BlessingKings:Exist(Player) and Spell.BlessingKings:Known() then
+			if Spell.BlessingofKingsG:Known() and Buff.BlessingofKingsG:Exist() then
+				if regularCast("BlessingofKingsG", Player) then
+					return true
+				end
+			else
+				if regularCast("BlessingKings", Player) then
+					return true
+				end
 			end
 		end
 	end
@@ -78,13 +104,37 @@ local function Buffing()
 		for _, Unit in ipairs(DMW.Units) do
 			unitclass, unitclassid = select(2, UnitClass(Unit.Pointer))
 			if Unit.Distance <= 10 and Unit.Player and Unit.Name~=Player.Name and Unit.LoS then
-				if might[unitclass] and not Buff.BlessingMight:Exist(Unit) then
-					if regularCast("BlessingMight",Unit) then
-						return true
+				if Spell.BlessingofKingsG:Known() or Spell.BlessingKings:Known() then
+					if Spell.BlessingofKingsG:Known() then
+						if regularCast("BlessingofKingsG", Unit) then
+							return true
+						end
+					else
+						if regularCast("BlessingKings", Unit) then
+							return true
+						end
 					end
-				elseif wisdom[unitclass] and not Buff.BlessingWissdom:Exist(Unit)then
-					if regularCast("BlessingWissdom",Unit) then
-						return true
+				else
+					if might[unitclass] and not Buff.BlessingMight:Exist(Unit) and Spell.BlessingMight:Known() then
+						if Spell.BlessingofMightG:Known() then
+							if regularCast("BlessingofMightG", Unit) then
+								return true
+							end
+						else
+							if regularCast("BlessingMight",Unit) then
+								return true
+							end
+						end
+					elseif wisdom[unitclass] and not Buff.BlessingWisdom:Exist(Unit) and Spell.BlessingWisdom:Known() then
+						if Spell.BlessingofWisdomG:Known() then
+							if regularCast("BlessingofWisdomG", Unit) then
+								return true
+							end
+						else
+							if regularCast("BlessingWisdom",Unit) then
+								return true
+							end
+						end
 					end
 				end
 			end
@@ -94,13 +144,13 @@ end
 
 local function Healing()
 	-- HolyLight out of Combat
-	if not Player.Combat and HP <= 75 then
+	if not Player.Combat and HP <= 75 and Spell.HolyLight:Known() then
 		if regularCast("HolyLight", Player) then
 			return true
 		end
 	end
 	-- HolyLight in Combat
-	if Player.Combat and HP <= 50 then
+	if Player.Combat and HP <= 50  and Spell.HolyLight:Known() then
 		if regularCast("HolyLight") then
 			return true
 		end
@@ -128,25 +178,25 @@ local function Combat ()
 			------- COMBAT -------
 			if Player.Combat and #Player:GetEnemies(5) >=1 and Target.Distance <= 10 then
 				--Seal of Righteousness
-				if Setting("Use Seal Of Righteousness") and not Buff.SealOfRight:Exist(Player) then
+				if Setting("Use Seal Of Righteousness") and not Buff.SealOfRight:Exist(Player) and Spell.SealOfRight:Known() then
 					if regularCast("SealOfRight",Player) then
 						return true
 					end
 				end
 				--Seal of Command
-				if Setting("Use Seal Of Command") and not Buff.SealOfRight:Exist(Player) then
+				if Setting("Use Seal Of Command") and not Buff.SealCommand:Exist(Player) and Spell.SealCommand:Known() then
 					if regularCast("SealCommand",Player) then
 						return true
 					end
 				end
 				--Seal of Crusader
-				if Setting("Use Seal Of Crusader") and not Buff.SealCrusader:Exist(Player) then
+				if Setting("Use Seal Of Crusader") and not Buff.SealCrusader:Exist(Player) and Spell.SealCrusader:Known() then
 					if regularCast("SealCrusader",Player) then
 						return true
 					end
 				end
 				--Judgement
-				if (Buff.SealOfRight:Exist(Player) or Buff.SealCrusader:Exist(Player) or Buff.SealCommand:Exist(Player)) and Spell.Judgement:CD() == 0 then
+				if (Buff.SealOfRight:Exist(Player) or Buff.SealCrusader:Exist(Player) or Buff.SealCommand:Exist(Player)) and Spell.Judgement:CD() == 0 and Spell.Judgement:Known() then
 					if regularCast("Judgement", Target) then
 						return true
 					end
@@ -156,12 +206,20 @@ local function Combat ()
 	end
 end
 
-local function Purify()
-	if Setting("Use Purify") and Spell.Purify:IsReady() then
+local function Dispel()
+	if Setting("Use Dispel") then
 		for _, Unit in ipairs(Player:GetFriends(40)) do
-			if Unit:Dispel(Spell.Purify) then
-				if regularCast("Purify",Player) then 
-					return true 
+			if not Spell.Cleanse:Known() then
+				if Unit:Dispel(Spell.Purify) then
+					if regularCast("Purify", Unit) then 
+						return true 
+					end
+				end
+			else
+				if Unit:Dispel(Spell.Cleanse) then
+					if regularCast("Cleanse", Unit) then 
+						return true 
+					end
 				end
 			end
 		end
@@ -191,7 +249,7 @@ function Paladin.Rotation()
 		return true
 	end
 	-- Dispel
-	if Purify() then
+	if Dispel() then
 		return true
 	end
 	--Start Combat
